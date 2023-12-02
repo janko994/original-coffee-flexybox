a220a0473fe2dfdf204f40f4efd8278b61dd8125
janko994 <jankopayacc@gmail.com>
2023-12-02 02:48:53 +0100

    test order

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -28,6 +28,34 @@ export default function App() {
     </AppProvider>
   );
 }
+const order = new shopify.rest.Order({session: session});
+order.line_items = [
+  {
+    "title": "Big Brown Bear Boots",
+    "price": 74.99,
+    "grams": "1300",
+    "quantity": 3,
+    "tax_lines": [
+      {
+        "price": 13.5,
+        "rate": 0.06,
+        "title": "State tax"
+      }
+    ]
+  }
+];
+order.transactions = [
+  {
+    "kind": "sale",
+    "status": "success",
+    "amount": 238.47
+  }
+];
+order.total_tax = 13.5;
+order.currency = "EUR";
+await order.save({
+  update: true,
+});
 
 // Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
